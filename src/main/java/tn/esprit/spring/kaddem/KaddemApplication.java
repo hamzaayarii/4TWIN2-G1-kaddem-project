960f44a37ac220d87b58bc1133e83a7b3f278c87
Amine2026 <mohamedamine.lazzez@esprit.tn>
2025-04-15 09:27:36 +0100

    test: adding comment to trigger webhook

--- src/main/java/tn/esprit/spring/kaddem/KaddemApplication.java
+++ src/main/java/tn/esprit/spring/kaddem/KaddemApplication.java
@@ -7,7 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @SpringBootApplication
 public class KaddemApplication {
-
+    // Webhook test comment - This is a test commit to trigger the webhook
     public static void main(String[] args) {
         SpringApplication.run(KaddemApplication.class, args);
     }
